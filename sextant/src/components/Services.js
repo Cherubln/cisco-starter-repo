dfe59e8de2611b4df1dd1bab1aa6669069c767c1
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2022-07-12 17:08:25 +0200

    Display ip versions separately

--- sextant/src/components/Services.js
+++ sextant/src/components/Services.js
@@ -1,9 +1,18 @@
+import ShowIP from "./ShowIP";
 import SingleService from "./SingleService";
 
 export default function Services() {
   return (
     <div className="services">
-      <SingleService name={"first service"}>
+      <SingleService name="IPV4 Address">
+        <ShowIP url={"https://api.ipify.org?format=json"} />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+        earum optio odio modi illo ipsum voluptatibus esse dolorum asperiores
+        soluta nostrum ipsa nemo consequuntur et eos, expedita repudiandae
+        voluptatum labore.
+      </SingleService>
+      <SingleService name="IPV6 Address">
+        <ShowIP url={"https://api64.ipify.org?format=json"} />
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
         earum optio odio modi illo ipsum voluptatibus esse dolorum asperiores
         soluta nostrum ipsa nemo consequuntur et eos, expedita repudiandae
--- sextant/src/components/ShowIP.js
+++ sextant/src/components/ShowIP.js
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
 
-export default function ShowIP({ version }) {
+export default function ShowIP({ url }) {
   const [addressIP, setAddressIP] = useState("");
 
   const getIP = async () => {
-    const url = version.toLowerCase().includes("ipv6")
-      ? "https://api64.ipify.org?format=json"
-      : "https://api.ipify.org?format=json";
     const response = await fetch(url);
     const { ip } = await response.json();
     setAddressIP(ip);
@@ -18,9 +15,7 @@ export default function ShowIP({ version }) {
 
   return (
     <div className="ip-container">
-      <div className="ip-label">
-        User's IP({version.toUpperCase()}) address:{" "}
-      </div>
+      <div className="ip-label">IP address: </div>
       <div className="ip-address">{!addressIP ? "Loading..." : addressIP}</div>
     </div>
   );
