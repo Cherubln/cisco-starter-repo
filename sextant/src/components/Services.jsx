6756bccdc90d369bea65ec658a2f098bf456e6c1
Cherubin Ndikubwayo <cherubinkubwayo@gmail.com>
2022-07-14 20:50:35 +0200

    Datastream from a netoworked service

--- sextant/src/components/Services.jsx
+++ sextant/src/components/Services.jsx
@@ -1,11 +1,17 @@
 import ShowIP from "./ShowIP";
 import SingleService from "./SingleService";
-
+import ShowLatency from "./ShowLatency";
 export default function Services() {
   return (
     <div className="services">
+      <SingleService name="IPV4 Address">
+        <ShowIP url={"https://api.ipify.org?format=json"} />
+      </SingleService>
       <SingleService name="IPV6 Address">
         <ShowIP url={"https://api64.ipify.org?format=json"} />
+      </SingleService>
+      <SingleService name="Packet Latency">
+        <ShowLatency />
       </SingleService>
     </div>
   );
--- sextant/src/components/SingleService.jsx
+++ sextant/src/components/SingleService.jsx
@@ -1,10 +1,7 @@
-import ShowLatency from "./ShowLatency";
-
 function SingleService({ name, children }) {
   return (
     <div className="service">
       <h3 className="service-title">{name}</h3>
-      <ShowLatency />
       <div>{children}</div>
     </div>
   );
